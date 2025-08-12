ce2862772bf08849fb07def19edb1107a3b295d4
mryuvi26 <yuvigalaxy7426@gmail.com>
2025-08-12 06:42:43 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Travelholic/',
+
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
   plugins: [react()],
   "@tailwindcss/postcss": {},
 })
