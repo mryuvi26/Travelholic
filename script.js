08c19b542e1858e8e84b60d4a49873116c1aaeb4
mryuvi26 <yuvigalaxy7426@gmail.com>
2025-08-18 10:28:17 +0530

    done

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const IMAGES = {
     image1: '/public/ChatGPT Image Aug 3, 2025, 11_43_27 AM.png',
-    image2: '/public/desert-6041013_1920.jpg'   
+    image2: '/public/desert-6041013_1920.jpg',
+    image3: '/public/four-seasons-2083680_1280.jpg',
+    image4: '/public/hawa-mahal-6688170_1920.jpg',
+    image5: '/public/indian-temple-4782290_1920.jpg',
+    image6: '/public/kishangarh-6511349_1920.jpg',
+    image7: '/public/Krishna_Janmabhoomi_Temple_Mathura.jpg'
 }
