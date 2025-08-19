e79864dbe676371214461d63a9cea128c60ed4d3
mryuvi26 <yuvigalaxy7426@gmail.com>
2025-08-19 17:18:34 +0530

    done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -14,9 +14,9 @@ import { useState } from 'react';
 
 const Slider = () => {
   const slides = [
-    <img src={`${import.meta.env.BASE_URL} ladakh-7545887_1920.jpg`} alt="" />,
-    <img src={`${import.meta.env.BASE_URL} pexels-souvenirpixels-417074.jpg`} alt="" />,
-    <img src={`${import.meta.env.BASE_URL} waterfall.jpg`} alt="" />
+    <img src={`${import.meta.env.BASE_URL}ladakh-7545887_1920.jpg`} alt="" />,
+    <img src={`${import.meta.env.BASE_URL}pexels-souvenirpixels-417074.jpg`} alt="" />,
+    <img src={`${import.meta.env.BASE_URL}waterfall.jpg`} alt="" />
 
   ]
   const [curr, setCurr] = useState(0);
